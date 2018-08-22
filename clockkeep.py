7d059bd88f3ee9144b4ac59473f691ba7d685e40
Dalek-Sec <Dalek-Sec@users.noreply.github.com>
2018-08-22 11:51:31 -0500

    Fill in functions but without validation

--- clockkeep.py
+++ clockkeep.py
@@ -8,13 +8,31 @@ times = db['times']
 notes = db['notes']
 
 def add_group(name, inactive=False):
-    pass
+    # TODO: validate conditions: group does not exist, name is valid str
+    groups.insert(name=name, inactive=False, punched_in=False)
 
-def punch_in(group, time=time.time()):
-    pass
+def punch_in(group_id, time=time.time()):
+    # TODO: validate conditions: group is punched out,
+    # no out times are None within group
+    db.begin()
+    times.insert(group_id=group_id, in_time=time, out_time=None)
+    group = groups.find_one(group_id=group_id)
+    group['punched_in'] = True
+    groups.update(group)
+    db.commit()
 
-def punch_out(group, time=time.time()):
-    pass
+def punch_out(group_id, time=time.time()):
+    # TODO: validate conditions: exactly one time in group is None,
+    # group is punched in
+    db.begin()
+    interval = times.find_one(group_id=group_id)
+    interval['out_time'] = time
+    times.update(interval)
+    group = groups.find_one(group_id=group_id)
+    group['punched_in'] = False
+    groups.update(group)
+    db.commit()
 
-def add_note(group, note):
-    pass
+def add_note(group_id, note, time=time.time()):
+    # TODO: validate group exists, note is valid str
+    notes.insert(group_id=group_id, time=time, note=note)
