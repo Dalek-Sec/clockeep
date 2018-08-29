13bfd1f9849704f132880e2a8fabaff12b16fd11
Dalek-Sec <Dalek-Sec@users.noreply.github.com>
2018-08-29 21:01:10 +0000

    Make reports prettier

--- ckcli.py
+++ ckcli.py
@@ -1,3 +1,5 @@
+import time
+from datetime import date, datetime, timedelta
 import clockkeep
 
 def print_info():
@@ -11,10 +13,45 @@ def print_info():
               ))
 
 def print_report(group_id):
-    times = clockkeep.times.find(group_id=group_id)
+    times = list(clockkeep.times.find(group_id=group_id, order_by='in_time'))
     print([time for time in times])
-    notes = clockkeep.notes.find(group_id=group_id)
+
+    time_matrix = get_time_matrix(times, 7)
+    print(time_matrix)
+
+    notes = list(clockkeep.notes.find(group_id=group_id, order_by='time'))
     print([note for note in notes])
+
+    for row in range(24):
+        time_row = ['_' if time_matrix[day][row] == 0 else '#' for day in range(7)]
+        time_row = ' |'.join(time_row)
+        print(str(row).rjust(3) + ':00  ' + time_row)
+
+    for note in notes:
+        print(time.ctime(note['time'])+': ' + note['note'])
+
+def get_time_matrix(times, days):
+    for row in times:
+        if row['out_time'] == None:
+            row['out_time'] = time.time()
+    time_matrix = []
+    today = date.today()
+    for days_back in range(days):
+        hours_list = []
+        for hour in range(24): # This got complicated to account for DST, but there are still odd edge cases
+            hour_start = datetime(today.year, today.month, today.day, hour) - timedelta(days=days_back)
+            hour_end = hour_start + timedelta(hours=1)
+            interval = [time.mktime(x.timetuple()) for x in [hour_start, hour_end]]
+            number = 1 if are_times_in_interval(interval, times) else 0
+            hours_list.append(number)
+        time_matrix.insert(0, hours_list)
+    return time_matrix
+
+def are_times_in_interval(interval, times):
+    for row in times:
+        if row['in_time'] < interval[1] and (row['out_time'] is None or row['out_time'] > interval[0]):
+            return True
+    return False
 
 while True:
     print_info()
--- clockkeep.py
+++ clockkeep.py
@@ -11,26 +11,32 @@ def add_group(name, inactive=False):
     # TODO: validate conditions: group does not exist, name is valid str
     groups.insert(dict(name=name, inactive=False, punched_in=False))
 
-def punch_in(group_id, time=time.time()):
+def punch_in(group_id, in_time=time.time()):
     # TODO: validate conditions: group is punched out,
     # no out times are None within group
     db.begin()
-    times.insert(dict(group_id=group_id, in_time=time, out_time=None))
+
+    times.insert(dict(group_id=group_id, in_time=in_time))
     group = groups.find_one(id=group_id)
     group['punched_in'] = True
     groups.update(group, ['id'])
+
     db.commit()
 
-def punch_out(group_id, time=time.time()):
+def punch_out(group_id, out_time=time.time()):
     # TODO: validate conditions: exactly one time in group is None,
     # group is punched in
     db.begin()
+
     interval = times.find_one(group_id=group_id)
-    interval['out_time'] = time
+    interval['out_time'] = out_time
+    print(interval)
     times.update(interval, ['id'])
+
     group = groups.find_one(id=group_id)
     group['punched_in'] = False
     groups.update(group, ['id'])
+
     db.commit()
 
 def toggle_punch(group_id, time=time.time()):
@@ -42,6 +48,6 @@ def toggle_punch(group_id, time=time.time()):
     else:
         punch_in(group_id, time)
 
-def add_note(group_id, note, time=time.time()):
+def add_note(group_id, note, note_time=time.time()):
     # TODO: validate group exists, note is valid str
-    notes.insert(dict(group_id=group_id, time=time, note=note))
+    notes.insert(dict(group_id=group_id, time=note_time, note=note))
